3120777bd959902be17a3f1718b4575aad07dec2
RissProg <ozinceidriscan@gmail.com>
2025-02-19 16:40:43 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PinPonGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            player = new PictureBox();
+            computer = new PictureBox();
+            ball = new PictureBox();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)player).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)computer).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ball).BeginInit();
+            SuspendLayout();
+            // 
+            // player
+            // 
+            player.Image = Properties.Resources.player;
+            player.Location = new Point(0, 150);
+            player.Name = "player";
+            player.Size = new Size(30, 120);
+            player.SizeMode = PictureBoxSizeMode.Zoom;
+            player.TabIndex = 0;
+            player.TabStop = false;
+            // 
+            // computer
+            // 
+            computer.Image = Properties.Resources.computer;
+            computer.Location = new Point(770, 150);
+            computer.Name = "computer";
+            computer.Size = new Size(30, 120);
+            computer.SizeMode = PictureBoxSizeMode.Zoom;
+            computer.TabIndex = 1;
+            computer.TabStop = false;
+            // 
+            // ball
+            // 
+            ball.Image = Properties.Resources.ball;
+            ball.Location = new Point(388, 211);
+            ball.Name = "ball";
+            ball.Size = new Size(50, 50);
+            ball.SizeMode = PictureBoxSizeMode.Zoom;
+            ball.TabIndex = 2;
+            ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += GameTimerEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ball);
+            Controls.Add(computer);
+            Controls.Add(player);
+            DoubleBuffered = true;
+            Name = "Form1";
+            Text = "Player: 0 -- Computer: 0";
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)player).EndInit();
+            ((System.ComponentModel.ISupportInitialize)computer).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ball).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox player;
+        private PictureBox computer;
+        private PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
